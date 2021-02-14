410ac64b103149d81cdb2374b17486a6b3235529
dmwanje <49172727+dmwanje@users.noreply.github.com>
2021-02-13 18:51:50 -0700

    Create main.js

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+// switch statements
+const grade = 'A';
+
+switch (grade){
+
+    case 'A':
+        console.log('You got an A');
+
+        console.log('You got an B');
+    
+    console.log('You got an C');
+
+console.log('You got an D');
+
+console.log('You got an E');
+
+}
